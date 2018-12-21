15839887cf0f9b7b047a330993025ed5a1e4bebb
givemin5 <givemin5@gmail.com>
2018-12-22 07:57:55 +0800

    Fifteen All Passed

--- TennisDay30/Tennis.cs
+++ TennisDay30/Tennis.cs
@@ -7,22 +7,25 @@ namespace TennisDay30
         private int _firstPlayerScoreTimes;
         private int _secondPlayerScoreTimes;
 
-        private Dictionary<int, string> _socreLookup = new Dictionary<int,string>
+        private Dictionary<int, string> _socreLookup = new Dictionary<int, string>
         {
-            {0,"Love" },
-            {1,"Fifteen" },
-            {2,"Thirty" },
-            {3,"Forty" },
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"},
         };
 
         public string Score()
         {
-            if (_secondPlayerScoreTimes>0||_firstPlayerScoreTimes>0)
+            if (_firstPlayerScoreTimes!=_secondPlayerScoreTimes)
             {
                 return $"{_socreLookup[_firstPlayerScoreTimes]} {_socreLookup[_secondPlayerScoreTimes]}";
             }
 
-
+            if (_firstPlayerScoreTimes == 1)
+            {
+                return "Fifteen All";
+            }
 
             return "Love All";
         }
