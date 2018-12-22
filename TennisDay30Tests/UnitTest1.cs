d5a77c1154ab21b1348d3571c354a3bea3bc2bf1
givemin5 <givemin5@gmail.com>
2018-12-22 08:01:23 +0800

    Deuce Fail

--- TennisDay30Tests/UnitTest1.cs
+++ TennisDay30Tests/UnitTest1.cs
@@ -71,6 +71,14 @@ namespace TennisDay30Tests
             GivenSecondPlayerScoreTimes(2);
             ScoreShouldBe("Thirty All");
         }
+
+        [TestMethod]
+        public void Deuce()
+        {
+            GivenFirstPlayerScoreTimes(3);
+            GivenSecondPlayerScoreTimes(3);
+            ScoreShouldBe("Deuce");
+        }
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (var i = 0; i < times; i++)
